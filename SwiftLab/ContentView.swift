bdc49290ca3344eca9abf0982bfa0c628d978d3e
Oleksandr Pylypenko <opilipenko@gmail.com>
2025-07-17 16:36:56 +0300

    Initial Commit

---
+++ SwiftLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftLab
+//
+//  Created by Oleksandr Pylypenko on 17.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftLab/SwiftLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLabApp.swift
+//  SwiftLab
+//
+//  Created by Oleksandr Pylypenko on 17.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
